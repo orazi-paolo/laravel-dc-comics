06901d2b6e40f362c55adcb9804ad0a92cd17c84
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-30 15:04:42 +0100

    created a route for the destroy method

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::post('/comics', [ComicController::class, 'store'])->name('comic.store');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comic.create');
 Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comic.show');
 Route::put('/comics/{id}', [ComicController::class, 'update'])->name('comic.update');
+Route::delete('/comics/{id}', [ComicController::class, 'destroy'])->name('comic.delete');
 Route::get('/comics/{id}/edit', [ComicController::class, 'edit'])->name('comic.edit');
