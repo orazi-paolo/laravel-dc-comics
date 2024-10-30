c9c982874d1ef8e5857459ada96ec799a1ba41c0
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-30 14:52:48 +0100

    Implemented new routes for edit and update

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/comics', [ComicController::class, 'index'])->name('comic.index');
-Route::get('/comics/create', [ComicController::class, 'create'])->name('comic.create');
 Route::post('/comics', [ComicController::class, 'store'])->name('comic.store');
+Route::get('/comics/create', [ComicController::class, 'create'])->name('comic.create');
 Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comic.show');
+Route::put('/comics/{id}', [ComicController::class, 'update'])->name('comic.update');
+Route::get('/comics/{id}/edit', [ComicController::class, 'edit'])->name('comic.edit');
