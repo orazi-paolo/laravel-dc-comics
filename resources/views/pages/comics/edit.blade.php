c1f507afe71b663e25242a0ddedb08237818fd71
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-31 16:09:44 +0100

    added values with old method in edit page

--- resources/views/pages/comics/edit.blade.php
+++ resources/views/pages/comics/edit.blade.php
@@ -21,35 +21,34 @@
     @endif
     <div class="mb-3">
         <label for="title" class="form-label">Title:</label>
-        <input type="text" name="title" class="form-control" placeholder="Enter the title" value="{{ $comic->title }}" required>
+        <input type="text" name="title" class="form-control" placeholder="Enter the title" value="{{ old('title', $comic->title) }}">
     </div>
 
     <div class="mb-3">
         <label for="description" class="form-label">Description:</label>
-        <input type="text" name="description" class="form-control" value="{{ $comic->description }}" placeholder="Enter the description">
+        <input type="text" name="description" class="form-control" placeholder="Enter the description" value="{{ old('description', $comic->description) }}">
     </div>
 
     <div class="mb-3">
         <label for="thumb" class="form-label">Thumb:</label>
-        <input type="text" name="thumb" class="form-control" value="{{ $comic->thumb }}" placeholder="Enter URL the thumb"></input>
+        <input type="text" name="thumb" class="form-control" placeholder="Enter URL the thumb" value="{{ old('thumb', $comic->thumb) }}">
     </div>
 
     <div class="mb-3">
         <label for="price" class="form-label">Price:</label>
-        <input type="text" name="price" class="form-control" placeholder="Enter the price" required value="{{ $comic->price }}">
+        <input type="text" name="price" class="form-control" placeholder="Enter the price" value="{{ old('price', $comic->price) }}">
     </div>
     <div class="mb-3">
         <label for="series" class="form-label">Series:</label>
-        <input type="text" name="series" class="form-control" value="{{ $comic->series }}" placeholder="Enter the series" required>
+        <input type="text" name="series" class="form-control" placeholder="Enter the series" value="{{ old('series', $comic->series) }}">
     </div>
     <div class="mb-3">
         <label for="sale_date" class="form-label">Sale date:</label>
-        <input type="text" name="sale_date" class="form-control" value="{{ $comic->sale_date }}" placeholder="Enter the sale date example: 2000/01/01"
-            required>
+        <input type="text" name="sale_date" class="form-control" placeholder="Enter the sale date example: 2000/01/01" value="{{ old('sale_date', $comic->sale_date) }}">
     </div>
     <div class="mb-3">
         <label for="type" class="form-label">Type:</label>
-        <input type="text" name="type" class="form-control" value="{{ $comic->type }}" placeholder="Enter the type" required>
+        <input type="text" name="type" class="form-control" placeholder="Enter the type" value="{{ old('type', $comic->type) }}">
     </div>
 
     <button type="submit" class="btn btn-primary w-100">Edit comic</button>
