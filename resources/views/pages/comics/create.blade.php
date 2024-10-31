83de76eb8c8cf11ee132d370be0f655b09bf6139
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-31 16:04:43 +0100

    added values ​​with old method in create page

--- resources/views/pages/comics/create.blade.php
+++ resources/views/pages/comics/create.blade.php
@@ -19,34 +19,34 @@
     @endif
     <div class="mb-3">
         <label for="title" class="form-label">Title:</label>
-        <input type="text" name="title" class="form-control" placeholder="Enter the title" required>
+        <input type="text" name="title" class="form-control" placeholder="Enter the title" value="{{ old('title') }}">
     </div>
 
     <div class="mb-3">
         <label for="description" class="form-label">Description:</label>
-        <input type="text" name="description" class="form-control" placeholder="Enter the description">
+        <input type="text" name="description" class="form-control" placeholder="Enter the description" value="{{ old('description') }}">
     </div>
 
     <div class="mb-3">
         <label for="thumb" class="form-label">Thumb:</label>
-        <input type="text" name="thumb" class="form-control" placeholder="Enter URL the thumb"></input>
+        <input type="text" name="thumb" class="form-control" placeholder="Enter URL the thumb" value="{{ old('thumb') }}"></input>
     </div>
 
     <div class="mb-3">
         <label for="price" class="form-label">Price:</label>
-        <input type="text" name="price" class="form-control" placeholder="Enter the price" required value="$">
+        <input type="text" name="price" class="form-control" placeholder="Enter the price" value="{{ old('price', '$') }}">
     </div>
     <div class="mb-3">
         <label for="series" class="form-label">Series:</label>
-        <input type="text" name="series" class="form-control" placeholder="Enter the series" required>
+        <input type="text" name="series" class="form-control" placeholder="Enter the series" value="{{ old('series') }}">
     </div>
     <div class="mb-3">
         <label for="sale_date" class="form-label">Sale date:</label>
-        <input type="text" name="sale_date" class="form-control" placeholder="Enter the sale date example: 2000/01/01" required>
+        <input type="text" name="sale_date" class="form-control" placeholder="Enter the sale date example: 2000/01/01" value="{{ old('sale_date') }}">
     </div>
     <div class="mb-3">
         <label for="type" class="form-label">Type:</label>
-        <input type="text" name="type" class="form-control" placeholder="Enter the type" required>
+        <input type="text" name="type" class="form-control" placeholder="Enter the type" value="{{ old('type') }}">
     </div>
 
     <button type="submit" class="btn btn-primary w-100">Add comic</button>
